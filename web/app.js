830401dd00e809eedb57477efd38215d4eebab34
tianez <saber_tz@163.com>
2016-08-08 17:39:20 +0800

    2

--- web/app.js
+++ web/app.js
@@ -53,20 +53,6 @@ const {
 require('./html/global')
 // require('./html/IndexedDB')
 
-function GetRequest() {
-    var url = location.search; //获取url中"?"符后的字串
-    var theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        strs = str.split("&");
-        for (var i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-        }
-    }
-    return theRequest;
-}
-window.GetRequest = GetRequest()
-
 function onEnter(nextState, replace) {
     let pathname = nextState.location.pathname
     let user = storedb('user').find() ? true : false
--- web/html/global.js
+++ web/html/global.js
@@ -9,12 +9,12 @@ window.ConfigActions = require('../flux/ConfigActions')
 window.ConfigStore = require('../flux/ConfigStore')
 
 //获取url参数数组
-window.get = function(url) {
+window.get = function (url) {
     if (!url) {
         var url = window.document.location.href.toString();
     }
     var u = url.split("?");
-    if (typeof(u[1]) == "string") {
+    if (typeof (u[1]) == "string") {
         u = u[1].split("&");
         var get = {};
         for (var i in u) {
@@ -28,7 +28,20 @@ window.get = function(url) {
 }
 
 //2个对象合并
-window.extend = function(o, n, override) {
+window.extend = function (o, n, override) {
     for (var p in n)
         if (n.hasOwnProperty(p) && (!o.hasOwnProperty(p) || override)) o[p] = n[p];
+}
+
+window.GetRequest = function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
 }
