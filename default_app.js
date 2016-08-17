c52c111343cf24c0dd1acd21db8a9e08e453c2d1
tianez <saber_tz@163.com>
2016-08-17 10:46:42 +0800

    1

--- default_app.js
+++ default_app.js
@@ -28,8 +28,8 @@ app.on('ready', function() {
         title: "王的理想乡",
         autoHideMenuBar: true
     })
-    mainWindow.loadURL('file://' +__dirname + '/web/home.html');
-    // mainWindow.loadURL('file://' +__dirname + '/../react/home.html');
+    // mainWindow.loadURL('file://' +__dirname + '/web/home.html');
+    mainWindow.loadURL('file://' +__dirname + '/../react/home.html');
     // mainWindow.loadURL('http://localhost:8888/');
     // mainWindow.webContents.openDevTools(); //打开开发者工具
     // mainWindow.setFullScreen(!mainWindow.isFullScreen());
